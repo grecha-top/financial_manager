76e8cf62552b052ca73d41484bd150fc6e0666b8
grecha-top <ugrechishnikova07@bk.ru>
2025-12-09 13:01:36 +0300

    Updated  PageController, TransactionsController, TransferController for dealing with famylies and invitations in families

--- controllers/TransactionsController.h
+++ controllers/TransactionsController.h
@@ -12,13 +12,15 @@ public:
     METHOD_LIST_BEGIN
         ADD_METHOD_TO(TransactionsController::createTransaction, "/transactions", drogon::Post);
         ADD_METHOD_TO(TransactionsController::GetTransactions, "/transactions", drogon::Get);
-        ADD_METHOD_TO(TransactionsController::GetTransactionById, "/transactions/{1}", drogon::Get);
-        ADD_METHOD_TO(TransactionsController::DeleteTransaction, "/transactions/{1}", drogon::Delete);
+        ADD_METHOD_TO(TransactionsController::GetTransactionById, "/transactions/{transactionId}", drogon::Get);
+        ADD_METHOD_TO(TransactionsController::UpdateTransaction, "/transactions/{transactionId}", drogon::Put);
+        ADD_METHOD_TO(TransactionsController::DeleteTransaction, "/transactions/{transactionId}", drogon::Delete);
     METHOD_LIST_END
 
     drogon::Task<drogon::HttpResponsePtr> createTransaction(drogon::HttpRequestPtr req);
     drogon::Task<drogon::HttpResponsePtr> GetTransactions(drogon::HttpRequestPtr req);
     drogon::Task<drogon::HttpResponsePtr> GetTransactionById(drogon::HttpRequestPtr req, int transactionId);
+    drogon::Task<drogon::HttpResponsePtr> UpdateTransaction(drogon::HttpRequestPtr req, int transactionId);
     drogon::Task<drogon::HttpResponsePtr> DeleteTransaction(drogon::HttpRequestPtr req, int transactionId);
 };
 
